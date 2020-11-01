18f21b80600824aa68f29259e8ed130b331b6eeb
benjdia <cravatas90@gmail.com>
2020-11-02 00:01:46 +0100

    Ajout des vues & utilisation de Bootswatch

---
+++ resources/views/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <title> blog | Laravel voyager</title>
+</head>
+<body>
+    
+    <script src="{{ asset('js/jquery.js')  }}"> </script>
+    <script src="{{ asset('js/bootstrap.min.js')  }}"> </script>
+    
+</body>
+</html>
